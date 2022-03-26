0050b04b72d9cfda916234edcc4bef482ea42143
Braden Kopenkoskey <bradenkopenkoskey@dhcp84-124.calvin.edu>
2022-03-26 10:46:50 -0400

    Add inequalities on image detection

--- main.py
+++ main.py
@@ -13,12 +13,17 @@ video_capture = cv2.VideoCapture(0)
 braden_image = face_recognition.load_image_file("bdkopen.png")
 braden_face_encoding = face_recognition.face_encodings(braden_image)[0]
 
+daveeid_image = face_recognition.load_image_file("daveeeid2.jpg")
+daveeid_face_encoding = face_recognition.face_encodings(daveeid_image)[0]
+
 # Create arrays of known face encodings and their names
 known_face_encodings = [
     braden_face_encoding,
+    daveeid_face_encoding,
 ]
 known_face_names = [
     "Braden Kopenkoskey",
+    "Daveeid"
 ]
 
 # Initialize some variables
@@ -36,8 +41,10 @@ while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
+    scale_size = 1
+
     # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    small_frame = cv2.resize(frame, (0, 0), fx=1/scale_size, fy=1/scale_size)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
@@ -52,49 +59,113 @@ while True:
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name = "Unknown"
+            name = "Target"
 
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
+            #     # # If a match was found in known_face_encodings, just use the first one.
+            #     # if True in matches:
+            #     #     first_match_index = matches.index(True)
+            #     #     name = known_face_names[first_match_index]
 
-            # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            # # Or instead, use the known face with the smallest distance to the new face
+            # face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
 
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
+            # best_match_index = np.argmin(face_distances)
+            # if matches[best_match_index]:
+            #     name = known_face_names[best_match_index]
 
             face_names.append(name)
 
     process_this_frame = not process_this_frame
 
     # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+    # for (top, right, bottom, left), name in zip(face_locations, face_names):
+    #     # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+    #     top *= scale_size
+    #     right *= scale_size
+    #     bottom *= scale_size
+    #     left *= scale_size
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+    #     # Draw a box around the face
+    #     cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+    #     # Draw a label with a name below the face
+    #     cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+    #     font = cv2.FONT_HERSHEY_DUPLEX
+    #     cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
     # Display the resulting image
-    cv2.imshow('Video', frame)
+    # cv2.imshow('Video', frame)
 
-    print(face_locations)
+    #Show downscaled
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        # top *= scale_size
+        # right *= scale_size
+        # bottom *= scale_size
+        # left *= scale_size
+
+        # Draw a box around the face
+        cv2.rectangle(small_frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(small_frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(small_frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        #circles	= cv2.HoughCircles(image, method, dp, minDist[, param1[, param2[, minRadius[, maxRadius]]]]])
+        # cv2.HoughCircles(small_frame, cv2.HOUGH_GRADIENT,)
+
+    # Display the resulting image
+    cv2.imshow('Video', small_frame)
+
+    if(len(face_locations)):
+        print(face_locations)
+
+    # If face location say it is centered
+    if(not len(face_locations)):
+        face_locations = [(640/scale_size/2, 480/scale_size/2, 640/scale_size/2, 480/scale_size/2)]
+
+    #This is on a 160 x 120 
+    face_locations_obj_top = face_locations[0][0]
+    face_locations_obj_right = face_locations[0][1]
+    face_locations_obj_bottom = face_locations[0][2]
+    face_locations_obj_left = face_locations[0][3]
+
+    face_locations_obj_face_width = face_locations[0][1] - face_locations[0][3]
+    face_locations_obj_face_height = face_locations[0][0] - face_locations[0][2]
+
+    print(face_locations_obj_left)
+
+    face_locations_obj_face_center_x = face_locations_obj_left + face_locations_obj_face_width/2
+    face_locations_obj_face_center_y = face_locations_obj_top + face_locations_obj_face_height/2
 
     ####################################
     # Motor Logic
     ####################################
 
+    #How many pixels of tolerance we want to give as error
+    tolerance = 5
+
+
+    # Check horizontal position
+    if(face_locations_obj_face_center_x > (160)/2 - tolerance and face_locations_obj_face_center_x < (160)/2 + tolerance):
+        print("Basically centered horizontally!")
+    elif(face_locations_obj_face_center_x > (160)/2):
+        print("Move to the left")
+    elif(face_locations_obj_face_center_x < (160)/2):
+        print("Move to the right")
+    else:
+        print("This line should never be reached")
+
+    # Check vertical position
+    if(face_locations_obj_face_center_y > (120)/2 - tolerance and face_locations_obj_face_center_y < (120)/2 + tolerance):
+        print("Basically centered vertically!")
+    elif(face_locations_obj_face_center_y > (120)/2):
+        print("Move down")
+    elif(face_locations_obj_face_center_y < (120)/2):
+        print("Move up")
+    else:
+        print("This line should never be reached")
 
 
     # Hit 'q' on the keyboard to quit!
