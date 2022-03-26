6369493c7400f52bc5e61047f6fed5fac3cda34b
Ben DeWeerd <bendeweerd2001@gmail.com>
2022-03-26 15:37:57 -0400

    update turret class

--- turret.py
+++ turret.py
@@ -8,18 +8,19 @@ class Turret():
         self.tiltStepper = Stepper(3, 4, 13, 26)
 
     def test(self):
-        self.camStepper.move(True, 10000, 0.0001)
-        self.camStepper.move(False, 10000, 0.0001)
+        self.camStepper.move(True, 10000, 0.0002)
+        self.camStepper.move(False, 10000, 0.0002)
 
     def adjust(self, dx, dy):
-        self.turnStepper.move(True, dx, 0.0001)
-        self.tiltStepper.move(True, dy, 0.0001)
+        self.turnStepper.move(True, dx, 0.0015)
+        self.tiltStepper.move(False, dy, 0.0002)
 
     def fire(self):
-        self.camStepper.move(True, 800, 0.0001)
+        self.camStepper.move(True, 800, 0.0003)
 
 if __name__ == "__main__":
     myTurret = Turret()
     while True:
-        myTurret.test()
+        myTurret.adjust(100,1000)
+        myTurret.fire()
         time.sleep(2)
