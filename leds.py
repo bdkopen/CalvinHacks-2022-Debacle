faf02398211730069c6d621ff9182cad3c239e65
Ben DeWeerd <bendeweerd2001@gmail.com>
2022-03-26 13:37:01 -0400

    add led control

---
+++ leds.py
@@ -0,0 +1,30 @@
+import board
+import neopixel
+import time
+
+class leds:
+    def __init__(self, num_pixels):
+        self.num_pixels = num_pixels
+        self.pixels = neopixel.NeoPixel(board.D18, num_pixels, auto_write = False, pixel_order=neopixel.GRB)
+    
+    def setColor(self):
+        self.pixels.fill((255,0,0))
+        self.pixels.show()
+
+    def setOff(self):
+        self.pixels.fill((0,0,0))
+        self.pixels.show()
+
+    def flash(self, duration):
+        self.setColor()
+        time.sleep(duration)
+        self.setOff()
+        time.sleep(duration)
+
+if __name__ == "__main__":
+    l = leds(10)
+    red = (255, 0, 0)
+    green = (0, 255, 0)
+    blue = (0, 0, 255)
+    while True:
+        l.flash(0.5)
